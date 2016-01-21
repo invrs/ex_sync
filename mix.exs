97cd4929c8ecdb7e15ab3b9dc1aa5bdfd009cc82
Michael Schaefermeyer <michael.schaefermeyer@gmail.com>
2016-01-21 17:20:37 +0100

    Add docs, add to hex

--- mix.exs
+++ mix.exs
@@ -5,30 +5,43 @@ defmodule ExSync.Mixfile do
     [app: :ex_sync,
      version: "0.0.1",
      elixir: "~> 1.2",
+     description: description,
+     package: package,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  def description do
+    """
+    A library to handle
+    [Differential Synchroniazation](https://neil.fraser.name/writing/sync/)
+    in an Elixir app.
+    """
+  end
+
+  def package do
+    [
+      files: ~w(lib mix.exs README.md LICENSE),
+      maintainers: ["Michael Schaefermeyer"],
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => "https://github.com/invrs/exsync",
+        "Docs" => "http://hexdocs.pm/exsync",
+      }
+    ]
+  end
+
   defp deps do
     [
-      {:connection, "~> 1.0"}
+      {:connection, "~> 1.0"},
+
+      {:earmark, "~> 0.2", only: :dev},
+      {:ex_doc, "~> 0.11", only: :dev},
     ]
   end
 end
