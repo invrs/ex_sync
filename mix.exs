52f49b55aecd846275534f7663eae8bc673f53ed
Michael Schaefermeyer <michael.schaefermeyer@gmail.com>
2016-03-16 15:29:30 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExSync.Mixfile do
 
   def project do
     [app: :ex_sync,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      description: description,
      package: package,
