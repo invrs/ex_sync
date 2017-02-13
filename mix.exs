3649af4d38a00339fa596196e9510eb3c1b3e3d2
Michael Schaefermeyer <michael.schaefermeyer@gmail.com>
2017-02-13 13:31:52 +0100

    Release 0.0.4

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExSync.Mixfile do
 
   def project do
     [app: :ex_sync,
-     version: "0.0.3",
+     version: "0.0.4",
      elixir: "~> 1.2",
      description: description(),
      package: package(),
